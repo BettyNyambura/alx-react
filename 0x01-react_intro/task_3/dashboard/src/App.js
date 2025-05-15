c436d7eecda13614e75c411c65709bbc4a72b5e7
Betty Waiyego <btynyambura20@gmail.com>
2025-05-15 17:37:20 +0300

    task 3

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <img src="https://alx-logo.png" alt="ALX logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password"> Password: </label>
+        <input type="password" id="password" name="password" />
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(false)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
